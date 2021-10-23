8ac23866a7ed4ce9757d4ae11ec33fb8ef037818
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-10-23 09:56:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MFProxy
+
+~~Mother F~~ Multi Factor Authentication proxy for mail protocols
+
+Inspiration was taken from [mfauth](https://github.com/rien/mfauth).
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "IMAP & SMTP proxy for multi-factor authentication";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            (rust-bin.stable.latest.default.override { extensions = [ "rls" "rust-src" ]; })
+            cargo-watch
+            openssl
+            pkg-config
+          ];
+          RUSTFLAGS = "-D warnings";
+        };
+      });
+}
